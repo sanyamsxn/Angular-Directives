fad5dcd48051ef5851c032a8ce6b1e942c589aa8
Sanyam Saxena <sanyamsxn1998@gmail.com>
2025-01-10 01:58:13 +0530

    UI part done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SigninComponent } from './signin/signin.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, NavigationComponent, SigninComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
